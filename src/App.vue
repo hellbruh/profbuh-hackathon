106a3ddd8ba76d737f3013a7263b72f4d205de0b
annya <annya.kkk@yandex.ru>
2023-06-27 19:26:37 +0500

    profbuh

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,155 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <div class="top">
+      <img class="pic" src="../public/icon.svg" width="80" height="63">
+      <div id="name">PROFBUH HACKATHON</div>
+    </div>
+    
+    <div class="instruction">
+        Введите ссылку и нажмите на кнопку, 
+        чтобы получить результат
+    </div>
+    
+    <div class="main">
+    
+      <div class="input-group mb-3" id="inp">
+        <span class="input-group-text" id="basic-addon1">URL</span>
+        <input type="text" v-model="link" class="form-control" placeholder="Введите ссылку" >
+      </div>
+      <div v-if="res==-1" id="valid">Поле не должно быть пустым</div>
+
+      <div class="input-group mb-3" id="inp">
+        <span class="input-group-text" id="basic-addon1">Количество символов  в статье</span>
+        <input type="text" v-model="stlen" class="form-control" placeholder="По умолчанию ограничений нет" aria-label="Имя пользователя" >
+      </div>
+
+      <div class="input-group mb-3" id="inp">
+        <span class="input-group-text" id="basic-addon1">Количество символов  в аннотации</span>
+        <input type="text" v-model="stan" class="form-control" placeholder="По умолчанию 500 символов" >
+      </div>
+
+      <div class="input-group mb-3" id="inp">
+        <span class="input-group-text" id="basic-addon1">Фрагмент видео (необязательно)</span>
+        <input type="text" v-model="timing" class="form-control" placeholder="Введите в формате чч:мм:сс чч:мм:сс" aria-label="Имя пользователя" aria-describedby="basic-addon1">
+      </div>
+      <button type="button" @click="showResult()" class="btn btn-primary" id="b">Сгенерировать</button>
+    </div>
+    
+    
+    <result v-if="res==1" ></result>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Result from './components/Result.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Result
+  },
+  data(){
+    return{
+      res:0,
+      stlen:'',
+      stan:'',
+      timing:'',
+      link:'',
+    }
+  },
+  methods:{
+    showResult(){
+      if (this.link.length>0){
+        this.res=1
+        if (this.stlen.length>0){
+          this.stlen=Number(this.stlen)
+        }
+        if (this.stan.length>0){
+          this.stan=Number(this.stan)
+        }
+        else{
+          this.stan = 500
+        }
+      }
+      else{
+        this.res=-1
+      }
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin:0%;
+  width:100%;
+  display:flex;
+  align-items:center;
+  flex-direction:column;
+}
+.top{
+  width:100%;
+  height:85px;
+  background-color: rgb(68, 194, 253);
+  font-family: 'Belanosima', sans-serif;
+  color:white;
+  font-size:28px;
+  display:flex; 
+  align-items:center;
+}
+#name{
+  margin-left:20px;
+  display:flex;
+}
+.pic{
+  display:flex;
+  margin-left:20px;
+}
+.main{
+  display:flex;
+  width:100%;
+  align-items:center;
+  margin-top:20px;
+  flex-direction:column;
+}
+.instruction{
+  display:flex;
+  font-family:'Roboto',sans-serif;
+  font-weight:100;
+  font-size:28px;
+  margin-top:100px;
+  flex-wrap:wrap;
+  width:40%;
+  text-align:center;
+}
+#inp{
+  width:40%;
+  display:flex;
+  flex-direction:row;
+  margin-top:15px;
+}
+#basic-addon1{
+  color:rgb(43, 43, 43);
+  width:285px;
+  display:flex;
+  flex-wrap:wrap;
+  text-align:center;
+}
+
+#valid{
+  display:flex;
+  color:rgb(194, 2, 2);
+  font-size:12px;
+  font-family:'Roboto',sans-serif;
+  margin-top:-15px;
+}
+#b{
+  background-color:rgb(60, 179, 235);
+  border-color:rgb(60, 179, 235); 
+}
+#b:hover{
+  background-color:rgb(4, 130, 189);
+  border-color:rgb(4, 130, 189); 
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,109 @@
+<template>
+    <div id="app">
+        <div class="error" v-if="material.length==0">
+            По данной ссылке ничего не найдено, 
+            либо в видео отсутствует содержательная информация.
+        </div>
+        <div style="display:flex; flex-direction:column; align-items:center">
+        <div v-if="material.length!=0">
+            <div v-for="i of material"
+            :key="i.id" id="cycle">
+                <img v-if="i.at(-1).includes('http')" 
+                    :src="i.at(-1)"
+                    class="pic">
+                <div v-if="!(i.at(-1).includes('http'))"
+                    class="input-group" id="inp">
+                    <span class="input-group-text" id="ddd">{{i[0]}}</span>
+                    <textarea class="form-control" :value="i.at(-1)" :v-model="i.at(-1)" placeholder="Оставьте поле пустым при необходимости, при обработке оно исчезнет"></textarea>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="down">
+            <button type="button" @click="saveResult()" class="btn btn-primary" id="b">Сохранить</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name:'ResultAlg',
+data(){
+    return{
+        material:[
+            ['00:01', 'Всем привет'],
+            ['00:1', '5 Проверяем присутствующих'],
+            ['https://image.winudf.com/v2/image/Y29tLm91cmxhYmVsLmN1dGVkb2d3YWxscGFwZXJfc2NyZWVuXzBfMTUzMzQwOTk1NF8wMDU/screen-0.jpg?fakeurl=1&type=.jpg'],
+            ['01:10', 'Всем пока']
+        ],
+        res:[],
+        test: [
+            ['00:01', 'Всем привет'],
+            ['00:1', '5 Проверяем присутствующих'],
+            ['https://image.winudf.com/v2/image/Y29tLm91cmxhYmVsLmN1dGVkb2d3YWxscGFwZXJfc2NyZWVuXzBfMTUzMzQwOTk1NF8wMDU/screen-0.jpg?fakeurl=1&type=.jpg'],
+            ['01:10', 'Всем пока']
+        ]
+
+    }
+},
+methods:{
+    saveResult(){
+        for (let object of this.material){
+            if(object.at(-1).length>0){
+                this.res.push(object)
+            }
+        }
+        console.log(this.res)
+    }
+    },
+
+}
+
+</script>
+
+<style scoped>
+#app{
+    display:flex;
+    flex-direction:column;
+    align-items: center;
+    width:40%;
+}
+.error{
+    width:100%;
+    margin-top:40px;
+    font-family:'Roboto',sans-serif;
+    font-weight:100;
+    font-size:24px;
+    text-align:center;
+}
+.pic{
+    filter: alpha(Opacity=91);
+    opacity: 0.91;
+    width:100%;
+    margin-top:20px;
+    margin-left:-4px;
+}
+#cycle{
+    display:flex;
+    justify-content:center;
+}
+
+#ddd{
+    width:80px;
+    text-align: center;
+}
+#inp{
+    margin-top:20px;
+    display:flex;
+    width:100%
+}
+.down{
+    height:150px;
+    display:flex;
+}
+#b{
+    height:40px;
+    margin-top:30px;
+    margin-bottom:30px;
+}
+</style>
